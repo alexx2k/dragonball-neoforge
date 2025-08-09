60776fc18c3fb2c03bb7dd5a8a322cbd3283458e
alexb <alex.balsillie@gmail.com>
2025-08-09 19:31:58 +0100

    init

---
+++ src/main/java/com/alex2k/dragonball/DragonBall.java
@@ -0,0 +1,52 @@
+package com.alex2k.dragonball;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(DragonBall.MOD_ID)
+public class DragonBall {
+    public static final String MOD_ID = "dragonball";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public DragonBall(IEventBus modEventBus, ModContainer modContainer) {
+
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
+            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+        }
+
+        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
+
+        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+        LOGGER.info("Hello from DragonBall mod!");
+
+    }
+}
---
+++ src/main/java/com/alex2k/dragonball/DragonBallClient.java
@@ -0,0 +1,29 @@
+package com.alex2k.dragonball;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+
+@Mod(value = DragonBall.MOD_ID, dist = Dist.CLIENT)
+
+@EventBusSubscriber(modid = DragonBall.MOD_ID, value = Dist.CLIENT)
+public class DragonBallClient {
+    public DragonBallClient(ModContainer container) {
+
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+        DragonBall.LOGGER.info("HELLO FROM DRAGONBALL CLIENT SETUP");
+        DragonBall.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
